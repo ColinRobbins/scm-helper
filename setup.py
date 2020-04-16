8e638004cb9aa706509351f5e6629f372462c042
ColinRobbins <colin.john.robbins@gmail.com>
2020-04-16 20:42:42 +0100

    black

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
+"""INstallation of SCM."""
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    DESCRIPTION = fh.read()
 
 setuptools.setup(
     name="scm",
@@ -9,28 +10,21 @@ setuptools.setup(
     author="Colin Robbins",
     author_email="colin.john.robbins@gmail.com",
     description="Helper tool to manage data in Swim Club Manager",
-    long_description=long_description,
+    long_description=DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/ColinRobbins/scm-helper",
-    entry_points = {
-        'console_scripts': ["scm=scm_helper.scm:main"]
-    },
+    entry_points={"console_scripts": ["scm=scm_helper.scm:main"]},
     project_urls={
         "Bug Tracker": "https://github.com/ColinRobbins/scm-helper/issues",
         "Documentation": "https://github.com/ColinRobbins/scm-helper/wiki",
         "Source Code": "https://github.com/ColinRobbins/scm-helper",
     },
     packages=setuptools.find_packages(),
-    install_requires=[
-      'schema',
-      'requests',
-      'pyyaml',
-      'cryptography',
-    ],
-classifiers=[
+    install_requires=["schema", "requests", "pyyaml", "cryptography",],
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
