6f7bb37ec2b48216d1df383d6aaf9cd2d253e434
ColinRobbins <colin.john.robbins@gmail.com>
2020-04-23 19:22:49 +0100

    python version assertion

--- scm-cmd.py
+++ scm-cmd.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """SCM wrapper - command line."""
 import sys
-sys.path.append("scm_helper/")
 
+sys.path.append("scm_helper/")
+assert sys.version_info >= (3, 7)
+
+# pylint: disable=wrong-import-position
 from scm_helper import main
 
 
--- scm.py
+++ scm.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """SCM wrapper."""
 import sys
+
 sys.path.append("scm_helper/")
+assert sys.version_info >= (3, 7)
 
 from scm_helper import main
 
