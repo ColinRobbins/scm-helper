a38612fc9c9ad3c109f9d9acd427054dfbe3b06f
ColinRobbins <colin.john.robbins@gmail.com>
2020-05-02 17:16:24 +0100

    isort

--- scm.py
+++ scm.py
@@ -2,10 +2,15 @@
 """SCM wrapper."""
 import sys
 
+
+# Black directive
+# fmt: off
 sys.path.append("scm_helper/")
 assert sys.version_info >= (3, 7)
 
+# pylint: disable=wrong-import-position
 from scm_helper import main
+
 
 
 if __name__ == "__main__":
