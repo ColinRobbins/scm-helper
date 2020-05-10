5e34309ad33b8d8207836fc0689a749c977639a6
ColinRobbins <colin.john.robbins@gmail.com>
2020-05-10 14:56:02 +0100

    backoff assert of ipad

--- scm.py
+++ scm.py
@@ -6,7 +6,7 @@ import sys
 # Black directive
 # fmt: off
 sys.path.append("scm_helper/")
-assert sys.version_info >= (3, 7)
+assert sys.version_info >= (3, 6)
 
 # pylint: disable=wrong-import-position
 from scm_helper import main
